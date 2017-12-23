f69938f8cdd62f7acada1d366f48fa88f8fe173d
Brandon Fowler <bmfowler4@gmail.com>
2017-12-23 11:41:23 -1000

    created buttons and did initial sanity check

---
+++ js/cash_register.js
@@ -0,0 +1,50 @@
+const zeroButton = document.getElementById('zero');
+const oneButton = document.getElementById('one');
+const twoButton = document.getElementById('two');
+const threeButton = document.getElementById('three');
+const fourButton = document.getElementById('four');
+const fiveButton = document.getElementById('five');
+const sixButton = document.getElementById('six');
+const sevenButton = document.getElementById('seven');
+const eightButton = document.getElementById('eight');
+const nineButton = document.getElementById('nine');
+
+zeroButton.addEventListener('click', () => {
+    console.log('zero clicked');
+});
+
+oneButton.addEventListener('click', () => {
+    console.log('one clicked');
+});
+
+twoButton.addEventListener('click', () => {
+    console.log('two clicked');
+});
+
+threeButton.addEventListener('click', () => {
+    console.log('three clicked');
+});
+
+fourButton.addEventListener('click', () => {
+    console.log('four clicked');
+});
+
+fiveButton.addEventListener('click', () => {
+    console.log('five clicked');
+});
+
+sixButton.addEventListener('click', () => {
+    console.log('six clicked');
+});
+
+sevenButton.addEventListener('click', () => {
+    console.log('seven clicked');
+});
+
+eightButton.addEventListener('click', () => {
+    console.log('eight clicked');
+});
+
+nineButton.addEventListener('click', () => {
+    console.log('nine clicked');
+});
